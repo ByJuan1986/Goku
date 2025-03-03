15f926e09fe9b2072b8b5f303bf5f549da6453fd
ceo_mbmd <oficialmbmd@gmail.com>
2025-03-02 22:37:03 -0300

    Update descargar-aplicacion.js

--- plugins/descargar-aplicacion.js
+++ plugins/descargar-aplicacion.js
@@ -14,7 +14,7 @@ async function downloadApk(id) {
 }
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) return conn.sendMessage(m.chat, {text: `⪩ _Ingrese el comando y el nombre de la aplicacion que desea buscar._\n\n• *Por ejemplo:*\n#apk2 WhatsApp`, { quoted: m });
+  if (!text) return conn.sendMessage(m.chat, {text: `⪩ _Ingrese el comando y el nombre de la aplicacion que desea buscar._\n\n• *Por ejemplo:*\n#apk2 WhatsApp`}, { quoted: m });
   try {
     const searchResults = await searchApk(text);
     const apkData = await downloadApk(searchResults[0].id);
@@ -24,13 +24,13 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 ❒ *Publicado en:* ${apkData.lastup}
 ❒ *Peso:* ${apkData.size}`;
     await conn.sendMessage(m.chat, { image: { url: apkData.icon }, caption: response }, { quoted: m });
-    await conn.sendMessage(m.chat, {text: `_Descargando, espere un momento..._`, { quoted: m })
+    await conn.sendMessage(m.chat, {text: `_Descargando, espere un momento..._`}, { quoted: m })
     if (apkData.size.includes('GB') || apkData.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, { text: 'Lo siento, la aplicacion pesa mucho, no podra ser enviado.' }, { quoted: m });
     }
     await conn.sendMessage(m.chat, { document: { url: apkData.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: apkData.name + '.apk', caption: null }, { quoted: m });
   } catch (e) {
-    await conn.sendMessage(m.chat, {text: `✎ _Ocurrio un error con el comando: *#${command}*_`, { quoted: m })
+    await conn.sendMessage(m.chat, {text: `✎ _Ocurrio un error con el comando: *#${command}*_`}, { quoted: m })
     console.log(e);
   }
 };

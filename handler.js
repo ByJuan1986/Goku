382f5479a02b2b3007f8adcf97004a56850ce921
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2025-03-14 15:28:57 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ import { unwatchFile, watchFile } from 'fs'
 import fs from 'fs'
 import chalk from 'chalk'   
 import fetch from 'node-fetch'
-import './plugins/_mdmx.js'
+import './plugins/_juan.js'
  
 /**
  * @type {import('@adiwajshing/baileys')}  
